4f3026a05fd52d7e96e0e8198d04e93788b1b2b3
Stockman-Jr <stockmanmimmi@gmail.com>
2025-04-10 11:09:49 +0200

    Fix connection error by changing broker url

--- react-mqtt-test/src/components/mqttDashboard.js
+++ react-mqtt-test/src/components/mqttDashboard.js
@@ -3,7 +3,7 @@ import { connectClient } from '../utils/client';
 import { subscribeToTopic, unsubscribeFromTopic } from '../utils/subscribe';
 import { publishToTopic } from '../utils/publisher';
 
-const brokerUrl = 'ws://broker.hivemq.com:8000/mqtt'; 
+const brokerUrl = 'ws://broker.emqx.io:8083/mqtt'; 
 
 export default function MqttDashboard() {
     const [topic, setTopic] = useState('test/topic');
@@ -31,6 +31,22 @@ export default function MqttDashboard() {
     return (
         <div>
             <h2>MQTT Dashboard Test</h2>
+            <button onClick={handleConnect}>Connect</button>
+            <div>
+                <input value={topic} onChange={(e) => setTopic(e.target.value)} />
+                <button onClick={handleSubscribe}>Subscribe</button>
+                <button onClick={handleUnsubscribe}>Unsubscribe</button>
+            </div>
+            <div>
+                <input value={message} onChange={(e) => setMessage(e.target.value)} />
+                <button onClick={handlePublish}>Publish</button>
+            </div>
+            <h3>Messages:</h3>
+            <ul>
+                {incomingMessages.map((msg, idx) => (
+                <li key={idx}>{msg}</li>
+                ))}
+            </ul>
         </div>
     );
 }
