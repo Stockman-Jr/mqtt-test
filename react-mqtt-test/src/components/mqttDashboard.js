862d1de6b0f62464bb4588f674fe9decf734aa6c
Stockman-Jr <stockmanmimmi@gmail.com>
2025-04-10 10:57:46 +0200

    Add dashboard component and handlers for client connection, topics and publishing

--- react-mqtt-test/src/components/mqttDashboard.js
+++ react-mqtt-test/src/components/mqttDashboard.js
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
+import { connectClient } from '../utils/client';
+import { subscribeToTopic, unsubscribeFromTopic } from '../utils/subscribe';
+import { publishToTopic } from '../utils/publisher';
 
+const brokerUrl = 'ws://broker.hivemq.com:8000/mqtt'; 
 
 export default function MqttDashboard() {
     const [topic, setTopic] = useState('test/topic');
+    const [incomingMessages, setIncomingMessages] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const handleConnect = () => {
+        connectClient(brokerUrl);
+    };
+
+    const handleSubscribe = () => {
+        subscribeToTopic(topic, (msg) => {
+            setIncomingMessages((prev) => [...prev, msg]);
+        });
+    };
+
+    const handleUnsubscribe = () => {
+        unsubscribeFromTopic(topic);
+    };
+
+    const handlePublish = () => {
+        publishToTopic(topic, message);
+    };
 
     return (
         <div>
